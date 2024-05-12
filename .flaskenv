61e61a42a10b9bc432d53837a1e4ff847b459437
rostik95 <danver1995@mail.ru>
2024-05-13 01:23:27 +0300

    initial setup

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=health_app.py
+FLASK_DEBUG=True
---
+++ app/config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+USER = os.environ.get("POSTGRES_USER")
+PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+HOST = os.environ.get("POSTGRES_HOST")
+PORT = os.environ.get("POSTGRES_PORT")
+DB = os.environ.get("POSTGRES_DB")
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
