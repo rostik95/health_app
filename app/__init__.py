5a90c84c0525911f18f6ca9ae07c8209aa836cbd
rostik95 <danver1995@mail.ru>
2024-05-18 15:08:48 +0300

    feat: added fonts

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .config import Config
+from .extensions import db, migrate
+from .routes.user import user
+from .routes.main import main
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+app.register_blueprint(user)
+app.register_blueprint(main)
